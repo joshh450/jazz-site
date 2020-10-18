528644cdf2526738cc4bf0b10860fda02605fc91
Josh Hougaard <joshh450@yahoo.com>
2020-10-18 14:13:17 -0600

    initial commit, created layout for background, navbar and themes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import NoteLogo from './assets/images/note-logo.png'
+import MountainLogo from './assets/images/mountain-logo.png'
+import CityLogo from './assets/images/city-logo.png'
 
 function App() {
+  const [mode, setMode] = useState('mountain')
+  let currentMode = mode === 'note' ? 'note' : mode === 'mountain' ? 'mountain' : 'city'
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`App ${currentMode}-background`}>
+      <nav className={`${currentMode}-navbar`}>
+        <img 
+          src={mode === 'note' ? NoteLogo : mode === 'mountain' ? MountainLogo : CityLogo} alt="Logo"
+          className={`${currentMode}-logo`}
+        />
+        <div className="mode-toggle">
+          <span 
+            className={`toggle ${currentMode === 'note' && 'note-toggle-active'}`} 
+            onClick={() => setMode('note')}
+          >
+            Note Mode
+          </span>
+          <span 
+            className={`toggle ${currentMode === 'mountain' && 'mountain-toggle-active'}`} 
+            onClick={() => setMode('mountain')}
+          >
+            Mountain Mode
+          </span>
+          <span 
+            className={`toggle ${currentMode === 'city' && 'city-toggle-active'}`} 
+            onClick={() => setMode('city')}
+          >
+              City Mode
+          </span>
+        </div>
+      </nav>
     </div>
   );
 }
